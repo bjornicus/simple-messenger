233a0a7248ed806740602ce6d65ad4fadc576a9a
Bjorn Hansen <holomorph@gmail.com>
2021-12-07 13:24:33 -0800

    allow cors for socket.io from locahost:3000

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+
+const io = new Server(server, {  
+    cors: {    
+        origin: "http://localhost:3000",    
+        methods: ["GET", "POST"]  
+    }
+});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
